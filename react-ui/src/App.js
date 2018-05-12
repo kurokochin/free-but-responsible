1ec569beced7a71339a45703572aa4fb33be6ffe
William Rumanta <rumantawilliam@yahoo.co.id>
2018-05-12 12:46:28 +0700

    init chart branch

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactChart from 'react-chartjs';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,28 +12,54 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
-    fetch('/api')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`status ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(json => {
-        this.setState({
-          message: json.message,
-          fetching: false
-        });
-      }).catch(e => {
-        this.setState({
-          message: `API call failed: ${e}`,
-          fetching: false
-        });
-      })
-  }
+  // componentDidMount() {
+  //   fetch('/api')
+  //     .then(response => {
+  //       if (!response.ok) {
+  //         throw new Error(`status ${response.status}`);
+  //       }
+  //       return response.json();
+  //     })
+  //     .then(json => {
+  //       this.setState({
+  //         message: json.message,
+  //         fetching: false
+  //       });
+  //     }).catch(e => {
+  //       this.setState({
+  //         message: `API call failed: ${e}`,
+  //         fetching: false
+  //       });
+  //     })
+  // }
 
   render() {
+    const LineChart = ReactChart.Line;
+    const data = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: "My First dataset",
+          fillColor: "rgba(220,220,220,0.2)",
+          strokeColor: "rgba(220,220,220,1)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: [90, 59, 80, 81, 56, 55, 40]
+        },
+        {
+          label: "My Second dataset",
+          fillColor: "rgba(151,187,205,0.2)",
+          strokeColor: "rgba(151,187,205,1)",
+          pointColor: "rgba(151,187,205,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(151,187,205,1)",
+          data: [28, 48, 40, 19, 86, 27, 90]
+        }
+      ]
+    };
     return (
       <div className="App">
         <div className="App-header">
@@ -50,6 +77,12 @@ class App extends Component {
             ? 'Fetching message from API'
             : this.state.message}
         </p>
+
+        <div>
+        asd
+        </div>
+
+        <LineChart data={data} width="600" height="250" />
       </div>
     );
   }
